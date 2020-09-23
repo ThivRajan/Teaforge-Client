35764a19e23213a2edb80cda8f1012c718062342
Thivagar Nadarajan <thiv.nadarajan@gmail.com>
2020-09-22 21:20:58 -0400

    added resistance rules

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -44,6 +44,10 @@ const Container = styled(ReactModal)`
 		margin-top: 15px;
 	}
 
+	b {
+		text-align: left;
+	}
+
 	u {
 		font-size: 27px;
 		text-align: left;
@@ -69,6 +73,7 @@ const CloseIcon = styled(FontAwesomeIcon)`
 
 	:hover {
 		filter: brightness(80%);
+		cursor: pointer;
 	}
 `;
 
@@ -83,10 +88,11 @@ const GameDescription: React.FC<{ modalOpen: boolean; closeModal: Function }> =
 				onRequestClose={() => closeModal()}
 				shouldFocusAfterRender={true}
 				shouldReturnFocusAfterClose={true}
+				ariaHideApp={false}
 			>
 				<CloseIcon icon={faTimes} onClick={() => closeModal()} />
 				<h1>Resistance</h1>
-				<i>Your goal is to either successfully complete or sabotage a mission</i>
+				<i>Your goal is to either succeed or sabotage the mission</i>
 				<div>
 					<Link to="/join">
 						<FilledButton color={blue}>Join Room</FilledButton>
@@ -96,41 +102,56 @@ const GameDescription: React.FC<{ modalOpen: boolean; closeModal: Function }> =
 					</Link>
 				</div>
 
-
 				<u>Rules</u>
 				<Players players={'5-10 Players'} />
 				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-					sed do eiusmod tempor incididunt ut labore et dolore magna
-					aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-					ullamco laboris nisi ut aliquip ex ea commodo consequat.
+					<b>
+						Players are allowed to talk at any point in the game, as long
+						as they are talking publicly (everyone must know what any one person
+						says).
+					</b>
+				</p>
+
+				<p>
+					There are two roles: Resistance or Spies. The Spies win
+					if three missions fail, and the Resistance win if three missions
+					succeed. Before the game begins, each player will be shown a message
+					telling them their role. If you are a spy, then you will also
+					be told who the other spies are.
 				</p>
 				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-					sed do eiusmod tempor incididunt ut labore et dolore magna
-					aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-					ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
-					aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-					cupidatat non proident, sunt in culpa qui officia deserunt
-					mollit anim id est laborum.
+					1. First, there is the team creation phase, where the team leader
+					will decide which players he/she wants on the mission. Note that
+					the team leader is rotated every turn (so the leader is always
+					a different person until everyone has been the leader).
 				</p>
 				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-					sed do eiusmod tempor incididunt ut labore et dolore magna
-					aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-					ullamco laboris nisi ut aliquip ex ea commodo consequat.
+					2. Once the leader has selected members, everyone will vote on
+					if they approve the team. If there is a majority vote of approval,
+					then you'll move on to the next phase. If there isn't a
+					majority vote, then there will be a different leader and the game
+					goes back to the team creation phase.
 				</p>
 				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-					sed do eiusmod tempor incididunt ut labore et dolore magna
-					aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-					ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
-					aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-					cupidatat non proident, sunt in culpa qui officia deserunt
-					mollit anim id est laborum.
+					3. Once the team has gotten majority approval, you will move to
+					the mission phase. Everyone on the team will get an option to either
+					succeed or fail the mission. If there is even a single person who selects
+					"fail", then the entire mission is considered to be a Fail. Once all
+					the team members have selected their option, you will be shown the mission
+					result (Success or Fail).
 				</p>
+				<p>
+					4. Now #1-3 will repeat until there are 3 successes (Resistance wins)
+					or 3 fails (Spies win).
+				</p>
+
+				<b>
+					Tip: If you are a spy, try to get either yourself or a fellow spy
+					to sneak onto the mission and fail it. If you are resistance, then try
+					to figure out who the spies are and stop them from going on the mission.
+				</b>
+
+
 			</Container>
 		);
 	};
