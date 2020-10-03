4f45fcd806fad16b9a62e5355128034ed8fd7dd3
Thivagar Nadarajan <thiv.nadarajan@gmail.com>
2020-10-03 13:14:44 -0400

    socket connections

--- src/components/CreateRoom.tsx
+++ src/components/CreateRoom.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import io from 'socket.io-client';
 
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import Form from '../styles/Form';
 import Button from '../styles/Button';
 
 import { Games } from '../types';
 
 const CreateForm: React.FC<{ game: Games }> = ({ game }) => {
-	const handleJoin = () => {
-		io.connect(`http://localhost:3001${game}`);
+	const history = useHistory();
+	const [name, setName] = useState('');
+
+	const handleCreate = (event: React.MouseEvent<HTMLButtonElement>) => {
+		event.preventDefault();
+		const socket = io.connect(`http://localhost:3001/${game}`);
+		socket.emit('create', name);
+		socket.on('roomKey', (room: string) => {
+			history.push(`/${game}/${room}`);
+		});
+		socket.on('players', (players: Array<string>) => {
+			console.log('PLAYERS: ', players);
+		});
 	};
 
 	return (
 		<>
 			<h1>Create Room</h1>
 			<Form>
-				<input placeholder="Name"></input>
+				<input placeholder="Name"
+					value={name} onChange={event => setName(event.target.value)}>
+				</input>
 				<br />
-				<Link to="/lobby">
-					<Button.Filled onClick={handleJoin}>Join</Button.Filled>
-				</Link>
+				<Button.Filled onClick={handleCreate}>Create</Button.Filled>
 				<Link to="/">
 					<Button.Outlined>Cancel</Button.Outlined>
 				</Link>
