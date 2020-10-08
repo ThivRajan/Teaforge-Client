ac8f0dd95beea012c9c52e75fc60fc425938cc3f
Thivagar Nadarajan <thiv.nadarajan@gmail.com>
2020-10-07 20:24:01 -0400

    add theme preference to local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './styles/Global';
@@ -8,13 +8,21 @@ import Home from './components/Home';
 import JoinForm from './components/JoinRoom';
 import CreateForm from './components/CreateRoom';
 import RoomLobby from './components/RoomLobby';
-// import Message from './components/Message';
 
 import { Game } from './types';
 
 const App = () => {
 	const [darkMode, setdarkMode] = useState(false);
-	const toggleTheme = (currTheme: boolean) => setdarkMode(!currTheme);
+	const toggleTheme = (currTheme: boolean) => {
+		setdarkMode(!currTheme);
+		const theme = darkMode ? 'light' : 'dark';
+		window.localStorage.setItem('theme', theme);
+	};
+
+	useEffect(() => {
+		if (window.localStorage.getItem('theme') === 'dark') setdarkMode(true);
+		else setdarkMode(false);
+	}, []);
 
 	return (
 		<ThemeProvider theme={{ darkMode }}>
