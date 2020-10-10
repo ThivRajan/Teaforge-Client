1a2f8eaef341953a85cbec6650ba336f8cdad57b
Thivagar Nadarajan <thiv.nadarajan@gmail.com>
2020-10-10 19:58:57 -0400

    set up mission board

--- src/components/games/Resistance/index.tsx
+++ src/components/games/Resistance/index.tsx
@@ -6,10 +6,15 @@ import { useStateValue } from '../../../state';
 
 //TODO: refactor board to be dynamic in respect to room size
 //TODO: considering making constants for the roles
+interface Mission {
+	players: number;
+	result: 'pass' | 'fail' | '';
+}
+
 const Resistance = () => {
 	const [{ socket, name, game, key },] = useStateValue();
 	const [role, setRole] = useState('');
-	const [missionBoard, setMissionBoard] = useState<number[]>([]);
+	const [missions, setMission] = useState<Mission[]>([]);
 
 	const history = useHistory();
 
@@ -17,7 +22,7 @@ const Resistance = () => {
 	useEffect(() => {
 		if (game && socket && key) {
 			socket.on('role', (role: string) => setRole(role));
-			socket.on('board', (board: number[]) => setMissionBoard(board));
+			socket.on('missions', (missions: Mission[]) => setMission(missions));
 			socket.emit('ready');
 		} else {
 			history.push('/join');
@@ -31,16 +36,47 @@ const Resistance = () => {
 	return (
 		<div>
 			<h1>Role: {role}</h1>
-			{
-				missionBoard.map(playerCount =>
-					<MissionCounter key={playerCount}>{playerCount}</MissionCounter>)
-			}
+			<MissionBoard>
+				{missions.map((mission, index) =>
+					<MissionResult key={index} result={mission.result}>
+						{mission.players}
+					</MissionResult>)
+				}
+			</MissionBoard>
+
 		</div>
 	);
 };
 
-const MissionCounter = styled.div`
-	border-radius: 5px;
+const MissionBoard = styled.div`
+	display: flex;
+	align-items: center;
+	margin: 10px;
+`;
+
+interface ResultProps {
+	result: 'pass' | 'fail' | '';
+}
+const MissionResult = styled.div<ResultProps>`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	// margin-left: 10px;
+	// margin-right: 10px;
+	margin: 8px;
+
+	height: 70px;
+	width: 70px;
+	font-size: 30px;
+	
+	border-radius: 50%;
+	border: 2px solid ${props => props.theme.darkMode ? 'white' : 'black'};
+	color: ${props => props.theme.darkMode ? 'white' : 'black'};
+
+	background: ${(props) => {
+		if (props.result) return props.result === 'pass' ? 'blue' : 'red';
+		return 'none';
+	}}
 `;
 
 export default Resistance;
