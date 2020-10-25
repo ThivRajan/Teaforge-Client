30c628c01b90e26cc4b912905378da5fa3cd70db
Thivagar Nadarajan <thiv.nadarajan@gmail.com>
2020-10-25 14:28:45 -0400

    add favicon

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,3 @@
-export const SERVER_URI = 'http://localhost:3001/';
-
-//TODO-DONE: change server to this
-// export const SERVER_URI = 'https://teaforge-server.herokuapp.com/';
+export const SERVER_URI = 'https://teaforge-server.herokuapp.com/';
 
 
