14c0249074dc5d84fe2eed8a45aca9d34fd37b01
Thivagar Nadarajan <thiv.nadarajan@gmail.com>
2023-05-08 16:23:30 -0400

    Update loading message to be generic

--- src/components/misc/Loader/Loader.tsx
+++ src/components/misc/Loader/Loader.tsx
@@ -4,7 +4,7 @@ import './Loader.css';
 const Loader = () => {
 	return (
 		<div className="loading">
-			Waiting for Heroku Server
+			Waiting for server
 			<span className="dot-1">.</span>
 			<span className="dot-2">.</span>
 			<span className="dot-3">.</span>
